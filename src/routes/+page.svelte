74a85b78e4a47922562f75dcf0cd4109747bd83f
Yuchen Cui <yuchencui@Yuchens-New-MacBookAir.local>
2026-01-23 16:07:25 -0800

    newS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,9 @@
   import ExternalLink from 'lucide-svelte/icons/external-link';
 
   const newsItems = [
-  {date: 'Jan 10, 2026',
-  parts: [
+  {
+    date: 'Jan 10, 2026',
+    parts: [
         { text: 'GAMMA', link: 'https://arxiv.org/abs/2601.05336' },
         { text: ' is on arXiv now' }
       ]
